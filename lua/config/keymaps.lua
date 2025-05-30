91d05d0035f9a3171e426eaf3bc455bd06343077
ferstormblessed <salvadorfernando2002@outlook.com>
2025-05-30 16:14:24 -0600

    add some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -74,3 +74,15 @@ set("n", "<M-s>", "<C-W>-", { desc = "decrease height of split" })
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<C-c>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<C-c><C-c>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
